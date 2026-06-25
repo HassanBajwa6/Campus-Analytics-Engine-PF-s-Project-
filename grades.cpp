0bbc37b1e88df8f4d69315ba8d41c6aa609cceaa
HASSAN SHAHZAD BAJWA <f2025376684@umt.edu.pk>
2026-06-25 23:10:49 +0500

    Update grades.cpp

--- grades.cpp
+++ grades.cpp
@@ -1,1 +1,61 @@
-C++
+#include "grades.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void addGrade()
+{
+    string id;
+    float quiz, assignment, mid, finalExam;
+    float total;
+
+    cout << "\nEnter Student ID: ";
+    cin >> id;
+
+    cout << "Quiz Marks: ";
+    cin >> quiz;
+
+    cout << "Assignment Marks: ";
+    cin >> assignment;
+
+    cout << "Mid Marks: ";
+    cin >> mid;
+
+    cout << "Final Marks: ";
+    cin >> finalExam;
+
+    total = (quiz * 0.10) +
+            (assignment * 0.10) +
+            (mid * 0.30) +
+            (finalExam * 0.50);
+
+    ofstream file("grades.txt", ios::app);
+
+    file << id << ","
+         << quiz << ","
+         << assignment << ","
+         << mid << ","
+         << finalExam << ","
+         << total << endl;
+
+    file.close();
+
+    cout << "\nGrade Added Successfully!\n";
+}
+
+void viewGrades()
+{
+    ifstream file("grades.txt");
+
+    string line;
+
+    cout << "\n===== GRADE RECORDS =====\n";
+
+    while(getline(file, line))
+    {
+        cout << line << endl;
+    }
+
+    file.close();
+}
