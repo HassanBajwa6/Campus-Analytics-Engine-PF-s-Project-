7a78bdbeb51b2f8275d65a7495bf4428ff0cf539
HASSAN SHAHZAD BAJWA <f2025376684@umt.edu.pk>
2026-06-25 23:02:52 +0500

    Update attendance.cpp

--- attendance.cpp
+++ attendance.cpp
@@ -1,1 +1,41 @@
-C++
+#include "attendance.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void markAttendance()
+{
+    string id, status;
+
+    cout << "\nEnter Student ID: ";
+    cin >> id;
+
+    cout << "Enter Attendance (P/A/L): ";
+    cin >> status;
+
+    ofstream file("attendance_log.txt", ios::app);
+
+    file << id << "," << status << endl;
+
+    file.close();
+
+    cout << "\nAttendance Marked Successfully!\n";
+}
+
+void viewAttendance()
+{
+    ifstream file("attendance_log.txt");
+
+    string line;
+
+    cout << "\n===== ATTENDANCE RECORDS =====\n";
+
+    while(getline(file, line))
+    {
+        cout << line << endl;
+    }
+
+    file.close();
+}
+
