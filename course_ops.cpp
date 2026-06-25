f1bf011501a064f46ec9cb22743f2808325b0172
HASSAN SHAHZAD BAJWA <f2025376684@umt.edu.pk>
2026-06-25 23:04:55 +0500

    Update course_ops.cpp

--- course_ops.cpp
+++ course_ops.cpp
@@ -1,1 +1,42 @@
+#include "course_ops.h"
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+
+void addCourse()
+{
+    string code, title;
+
+    cout << "\nEnter Course Code: ";
+    cin >> code;
+
+    cin.ignore();
+
+    cout << "Enter Course Title: ";
+    getline(cin, title);
+
+    ofstream file("courses.txt", ios::app);
+
+    file << code << "," << title << endl;
+
+    file.close();
+
+    cout << "\nCourse Added Successfully!\n";
+}
+
+void viewCourses()
+{
+    ifstream file("courses.txt");
+
+    string line;
+
+    cout << "\n===== COURSE RECORDS =====\n";
+
+    while(getline(file, line))
+    {
+        cout << line << endl;
+    }
+
+    file.close();
+}
