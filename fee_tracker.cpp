e5a5bbfeae3fac12991f547c24bd766386d2ce99
HASSAN SHAHZAD BAJWA <f2025376684@umt.edu.pk>
2026-06-25 23:07:55 +0500

    Update fee_tracker.cpp

--- fee_tracker.cpp
+++ fee_tracker.cpp
@@ -1,1 +1,49 @@
-C++
+#include "fee_tracker.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void addFee()
+{
+    string id;
+    float totalFee, paidFee, balance;
+
+    cout << "\nEnter Student ID: ";
+    cin >> id;
+
+    cout << "Enter Total Fee: ";
+    cin >> totalFee;
+
+    cout << "Enter Paid Fee: ";
+    cin >> paidFee;
+
+    balance = totalFee - paidFee;
+
+    ofstream file("fees.txt", ios::app);
+
+    file << id << ","
+         << totalFee << ","
+         << paidFee << ","
+         << balance << endl;
+
+    file.close();
+
+    cout << "\nFee Record Added Successfully!\n";
+}
+
+void viewFees()
+{
+    ifstream file("fees.txt");
+
+    string line;
+
+    cout << "\n===== FEE RECORDS =====\n";
+
+    while(getline(file, line))
+    {
+        cout << line << endl;
+    }
+
+    file.close();
+}
